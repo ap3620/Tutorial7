e2b5e7ecdc9338f1c48919df46ed15f99cbde07d
Anoja Paraniroopasingam <ap3620@ic.ac.uk>
2022-11-22 16:46:48 +0000

    t71

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,22 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
     }
+
+        {
+            int s=0;
+            for (int i=30;i<=40;i+=1)
+                s+=i;
+            System.out.println(s);
+        }
+    }
+
 }
+
